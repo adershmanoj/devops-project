71c9139d13792850603fabb99c808cbaf536d7ff
adershmanoj <adershmanoj@gmail.com>
2018-10-10 14:54:43 +0530

    Fixed syntax error in AddNumbersTest.java

--- src/main/java/com/nissan/AddNumbersTest.java
+++ src/main/java/com/nissan/AddNumbersTest.java
@@ -3,7 +3,6 @@ package com.nissan;
 import junit.framework.TestCase;
 
 public class AddNumbersTest extends TestCase {
-    @Test
     public void testAddNumbers() {
 	AddNumbersServlet addNumbersServlet = new AddNumbersServlet();
 	double result = addNumbersServlet.addNumbers(5, 10);
